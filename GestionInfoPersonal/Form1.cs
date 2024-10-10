bfeb07acc369b4db6fb3ec832c620e93b4b44d00
Raúl Navarro Gallego <rnavarrog06@educastillalamancha.es>
2024-10-10 14:26:27 +0200

    Añadir funcionamiento al botón limpiar y otras funciones
    Se ha añadido un método para limpiar todos los campos y se ha añadido un temporizador y el ArrayList del combobox.

--- GestionInfoPersonal/Form1.cs
+++ GestionInfoPersonal/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,9 +28,57 @@ namespace GestionInfoPersonal
             DialogResult rs = MessageBox.Show("¿Limpiar todos los campos?", "Limpiar campos", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (rs == DialogResult.Yes)
             {
-
+                limpiar();
             }
         
+        }
+
+        private void limpiar()
+        {
+            tboxNombre.Text = String.Empty;
+            tboxDireccion.Text = String.Empty;
+            tboxCorreo.Text = String.Empty;
+
+            rbHombre.Checked = false;
+            rbMujer.Checked = false;
+            rbOtro.Checked = false;
+
+            mcNacimiento.SetDate(DateTime.Now);
+            nudEdad.Value = 1;
+
+            cbBoletin.Checked = false;
+            cbAlcohol.Checked = false;
+            cbDulces.Checked = false;
+
+            while (chlbHobbies.CheckedIndices.Count > 0) { 
+
+                chlbHobbies.SetItemChecked(chlbHobbies.CheckedIndices[0], false);
+
+            }
+
+            tbSatisfaccion.Value = 0;
+            tbRecomendar.Value = 0;
+
+            pboxImagen.Image = null;
+
+        }
+
+        private void mcNacimiento_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            lblNacimiento.Text = mcNacimiento.SelectionStart.ToString("dd/MM/yyyy");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArrayList paises = new ArrayList {"España", "Francia", "Portugal", "Alemania", "Paises Bajos", "Inglaterra"};
+            cboxPais.DataSource = paises;
+
+            tempFecha.Start();
+        }
+
+        private void tempFecha_Tick(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToString();
         }
     }
 }
